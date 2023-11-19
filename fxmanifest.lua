0266a509711c9404f3ff5fb28170f2997df3b5f7
xViperAG <usairsoftingandgaming@gmail.com>
2023-11-19 01:08:17 -0500

    quick fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ version '1.0.0'
 author 'JDev / xViperAG'
 
 shared_scripts { '@ox_lib/init.lua', '@qbx_core/modules/utils.lua', 'config.lua', '@qbx_core/shared/locale.lua', 'locales/en.lua', 'locales/*.lua' }
-client_scripts { '@qbx_core/modules/playerdata', 'client/main.lua' }
+client_scripts { '@qbx_core/modules/playerdata.lua', 'client/main.lua' }
 server_scripts { '@oxmysql/lib/MySQL.lua', 'server/main.lua' }
 
 lua54 'yes'
