cf56e06a968b2527f6b4b1aa86e8dea4170296e9
daddyDUBZ <82202019+daddyDUBZ@users.noreply.github.com>
2024-01-28 03:31:44 -0700

    Revert "Update main.lua"
    This reverts commit 96b7bec0da81d8e2e842812d73ff0710ff601df7.

--- client/main.lua
+++ client/main.lua
@@ -10,6 +10,8 @@ local VehicleClassMap = {}
 local config = require 'config.client'
 local Garages = require 'config.shared'.Garages
 local HouseGarages = require 'config.shared'.HouseGarages
+local UpdateRadial = false
+local ParkingUpdated = false
 
 -- helper functions
 local function TableContains(tab, val)
@@ -970,14 +972,28 @@ CreateThread(function()
                     end
                     UpdateRadial = false
                 end,
+                inside = function (self)
                     while self.insideZone do
                         Wait(2500)
                         if self.insideZone then
-                            lib.onCache('seat', function(value)
+                            local ClosestVehicle = lib.getClosestVehicle(GetEntityCoords(cache.ped), config.VehicleParkDistance)
+                            if UpdateRadial then
                                 UpdateRadialMenu(garageName)
-                            end)
+                                UpdateRadial = false
+                            else
+                                if ClosestVehicle and not ParkingUpdated then
+                                    UpdateRadial = true
+                                    ParkingUpdated = true
+                                else
+                                    if ParkingUpdated and not ClosestVehicle then
+                                        ParkingUpdated = false
+                                        lib.removeRadialItem('park_vehicle')
+                                    end
+                                end
+                            end
                         end
                     end
+                end,
                 onExit = function()
                     ResetCurrentGarage()
 					RemoveRadialOptions()
@@ -1019,3 +1035,10 @@ CreateThread(function()
     end
 end)
 
+AddEventHandler('baseevents:enteredVehicle', function(vehicle)
+    UpdateRadial = true
+end)
+
+AddEventHandler('baseevents:leftVehicle', function(vehicle)
+    UpdateRadial = true
+end)
